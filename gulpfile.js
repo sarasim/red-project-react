7729c52fabcbc49853a5e7e4f578d3c701636249
Sara Sim <sarasim@Saras-MacBook-Air.local>
2016-02-18 14:10:01 -0800

    Initiate webpack and update main.jsx with markup.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,59 @@
 // 'use strict';
+//plugins
 
 var gulp = require('gulp');
-var babel = require('gulp-babel');
 var browserify = require('gulp-browserify');
 var browserSync = require('browser-sync');
+var babelLoader = require ('babel-loader');
 var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var webpack = require('webpack-stream');
 
 var sass = require('gulp-sass');
-    autoprefixer = require('gulp-autoprefixer');
     minifyCSS = require('gulp-minify-css');
     rename = require('gulp-rename');
 
+//paths
 
-gulp.task('compile-react', function() {
-	return gulp.src('main.jsx')
-	  .pipe(plumber())
-		.pipe(babel({
-			presets: ['es2015', 'react']
-		}))
-		.pipe(browserify({
-			insertGlobals: true,
-			debug: true
-		}))
-		.pipe(gulp.dest('./'));
+// var jsMain
+// var sassMain
+// var htmlPath
+//
+// var basePath
+// var buildPath
+
+
+// gulp.task('compile-react', function() {
+// 	return gulp.src('main.jsx')
+// 	  .pipe(plumber())
+// 		.pipe(babel({
+// 			presets: ['es2015', 'react']
+// 		}))
+// 		.pipe(browserify({
+// 			insertGlobals: true,
+// 			debug: true
+// 		}))
+// 		.pipe(gulp.dest('./build'));
+// });
+
+gulp.task('compile-js', function() {
+        gulp.src('./main.jsx')
+          .pipe(webpack({
+            output: {
+              filename: 'main.js'
+            },
+            module: {
+              loaders: [{
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                  presets: ['react','es2015']
+                }
+              }]
+            },
+          }))
+          .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('sass', function(){
@@ -36,7 +67,7 @@ gulp.task('sass', function(){
   .pipe(gulp.dest('./build/css'));
   });
 
-gulp.task('browser-sync', ['compile-react'], function() {
+gulp.task('browser-sync', ['compile-js'], function() {
 
 	browserSync.init({
 		server: './'
@@ -44,8 +75,8 @@ gulp.task('browser-sync', ['compile-react'], function() {
 
 
   gulp.watch(['./scss/*.scss'], ['sass']);
-	gulp.watch(['main.jsx'], ['compile-react']);
-	gulp.watch(['main.js', 'index.html', './build/css/style.min.css']).on('change', browserSync.reload);
+	gulp.watch(['main.jsx'], ['compile-js']);
+	gulp.watch(['./build/main.js', 'index.html', './build/css/style.min.css']).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['browser-sync', 'sass']);
--- main.jsx
+++ main.jsx
@@ -8,17 +8,21 @@ var Mars = React.createClass({
 
   render: function(){
     return (
-//       <div className="mars-quiz">
-//         <header className="header">
-//            <h1>Mars</h1>
-//          </header>
-//         <button className="takeTest" onClick={}>Take Test</button>
-//          <i className="fa fa-space-shuttle"></i>
-//          <div className="clock"></div>
-//        </div>
-//     )
-//   }
-// });
+      <div className="mars-quiz">
+        <div className="side-panel">
+           <h1 className="mars-header">Mars</h1>
+           <div className="shuttle"><i className="fa fa-space-shuttle"></i></div>
+        </div>
+        <div className="main-panel">
+            <button className="takeTest">Take Test</button>
+            <div className="timer">
+              <p>00:00</p>
+            </div>
+        </div>
+      </div>
+    )
+  }
+});
 
 //end
 ReactDOM.render(<Mars />, document.querySelector('#mount-node'));
